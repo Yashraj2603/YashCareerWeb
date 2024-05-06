e5ebc00ef3e23cbcc9914b30e82af4593efb83eb
Yash Raj <55595124+Yashraj2603@users.noreply.github.com>
2024-05-06 11:28:30 +0530

    Update urls.py
    jobportals.url added

--- YashCarreerWebsite/YashCarreerWebsite/urls.py
+++ YashCarreerWebsite/YashCarreerWebsite/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',include("authentication.urls")),
+    path('JobPortal/' , include("JobPortal.urls")),
     path('',include("OrganisationEmployee.urls")),
 ]
